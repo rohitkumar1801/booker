b216affac4a15a966136477631834ee73b1f4b17
Rohit Kumar <rohitkumar.ff14@gmail.com>
2024-09-26 08:00:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware configuration
 app.use(cors({
-  origin: process.env.FRONTEND_URL, 
+  origin: 'https://booker-front.vercel.app', 
   credentials: true, 
 }));
 app.use(express.json());
